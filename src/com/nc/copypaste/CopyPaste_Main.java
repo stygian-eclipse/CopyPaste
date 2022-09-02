0311b48566feb58884e50b895b72c50adc5c8ebe
Nicolae Codleanu <nicolae.codleanu@cerner.com>
2022-09-02 16:53:43 +0300

    first commit

---
+++ src/com/nc/copypaste/CopyPaste_Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.nc.copypaste;
+
+/**
+ * @author NC041705
+ *
+ */
+public class CopyPaste_Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author NC041705
+ *
+ */
+module CopyPaste {
+}
